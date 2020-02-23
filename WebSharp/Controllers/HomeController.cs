a1aba409f16c72445638735f2ad236a920c35142
HkingAuditore <178854663@qq.com>
2020-02-23 21:14:43 +0800

    添加web与窗体

---
+++ WebSharp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSharp.Controller
+{
+    public class HomeController : System.Web.Mvc.Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Hello World";
+        }
+    }
+}
--- WindowsSharp/Form1.cs
+++ WindowsSharp/Form1.cs
@@ -8,5 +8,15 @@ namespace WindowsSharp
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            
+        }
     }
 }
